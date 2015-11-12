83dc7edf0a54d32b7abf8274b8c6ccc0ca483215
Robert Coleman <github@robert.net.nz>
2015-11-13 12:28:08 +1300

    fix specs

--- spec/foxycart_helpers_spec.rb
+++ spec/foxycart_helpers_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe FoxycartHelpers do
   it 'has a version number' do
-    expect(Foxycart::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(FoxycartHelpers::VERSION).not_to be nil
   end
 end
