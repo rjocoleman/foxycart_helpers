396f67f1721375492d534a11ca5eb82da8d49285
Robert Coleman <github@robert.net.nz>
2015-11-14 22:09:22 +1300

    rails view helpers

--- lib/foxycart_helpers/railtie.rb
+++ lib/foxycart_helpers/railtie.rb
@@ -1,10 +1,17 @@
 require 'foxycart_helpers/middleware'
+require 'foxycart_helpers/rails_view_helpers'
 
 module FoxycartHelpers
   class Railtie < Rails::Railtie
 
     initializer 'foxycart_helpers.use_rack_middleware' do |app|
       app.config.middleware.use 'FoxycartHelpers::Middleware'
+    end
+
+    initializer 'foxycart_helpers.configure_view_controller' do |app|
+      ActiveSupport.on_load :action_view do
+        include FoxycartHelpers::RailsViewHelpers
+      end
     end
 
   end
