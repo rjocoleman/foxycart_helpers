c2ba6f51152a6f2001c201dddbcc7b3f25cda45f
Robert Coleman <github@robert.net.nz>
2015-11-14 21:46:50 +1300

    add return of the fully formed encoded string

--- lib/foxycart_helpers/product_verification.rb
+++ lib/foxycart_helpers/product_verification.rb
@@ -3,13 +3,38 @@ require 'foxycart_helpers/configuration'
 module FoxycartHelpers
   class ProductVerification
 
-    def self.encode(code, name, value)
+    def self.encode(*args)
+      new(*args).encode
+    end
+
+    def self.encoded_name(*args)
+      new(*args).encoded_name
+    end
+
+    def encode
       digest = OpenSSL::Digest.new 'sha256'
-      key = FoxycartHelpers.configuration.api_key
-      data = code + name + value
+      key = config.api_key
+      data = @code + @name + @value
 
       OpenSSL::HMAC.hexdigest digest, key, data
     end
+
+    def encoded_name
+      @name + '||' + encode
+    end
+
+    def config
+      FoxycartHelpers.configuration
+    end
+
+    def initialize(code, name, value)
+      @code = code
+      @name = name
+      @value = value
+    end
+
+    # encoded_value is an alias for encoded_name
+    singleton_class.send :alias_method, :encoded_value, :encoded_name
 
   end
 end
